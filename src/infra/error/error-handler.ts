8403b89d5fe7b4634f311f651ac6d34933399f37
João Victor Olivo <jvso@yahoo.com>
2025-05-01 22:57:02 -0300

    More readable generic error handler

--- src/infra/error/error-handler.ts
+++ src/infra/error/error-handler.ts
@@ -2,30 +2,37 @@ import express from 'express';
 import * as logger from '../../utils/logger';
 import { HttpError, HttpErrorHandler } from './error-classes';
 
-export function jsonParserHandler(err: unknown) {
+export function jsonParserHandler(err: unknown, _req: express.Request, _res: express.Response, _next: express.NextFunction) {
 	if (err instanceof SyntaxError && 'body' in err)
 		throw new HttpError(400, `Malformed JSON in Request Body: ${err.message}`, err);
 	throw err;
 }
 
-export function errorHandler(err: unknown, req: express.Request, res: express.Response, _next: express.NextFunction) {
-	let httpError: HttpError;
+function logError(req: express.Request, httpError: HttpError, err: unknown) {
+	if (!(err instanceof Error)) {
+		logger.fatal(req, "A non-error landed on errorHandler.", httpError);
+		logger.warn(req, "Data landed on handler:", err);
+	} else if (httpError.statusCode >= 500) {
+		logger.error(req, httpError);
+	} else if ([401, 403].includes(httpError.statusCode)) {
+		logger.warn(req, httpError);
+	} else logger.debug(req, httpError);
 
-	if (err instanceof HttpError)
-		httpError = err;
-	else if (err instanceof Error)
-		httpError = new HttpError(500, err.message, err);
-	else
-		httpError = new HttpError(500);
+	if (err instanceof Error)
+		logger.trace(req, "Error landed on handler:", err);
+}
+
+export function errorHandler(err: unknown, req: express.Request, res: express.Response, _next: express.NextFunction) {
+	const httpError = err instanceof HttpError
+		? err
+		: err instanceof Error
+			? new HttpError(500, err.message, err)
+			: new HttpError(500);
 
 	const handler = new HttpErrorHandler(httpError);
-
 	logger.error(req, handler);
-	if (httpError.statusCode < 400 || httpError.statusCode >= 500)
-		logger.error(req, httpError);
-	else if ([401, 403, 404].includes(httpError.statusCode))
-		logger.warn(req, httpError);
-	else logger.debug(req, httpError);
+
+	logError(req, httpError, err);
 
 	res.status(handler.status).json(handler);
 }
