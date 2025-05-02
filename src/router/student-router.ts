29f1f79d4c6a752f0f2a460b2881add263d80b2b
João Victor Olivo <jvso@yahoo.com>
2025-05-02 17:36:05 -0300

    Removed custom Catcher from router. Built-in express since v5.0

--- src/router/student-router.ts
+++ src/router/student-router.ts
@@ -3,27 +3,10 @@ import * as Student from "../controller/student-controller";
 
 export const studentRouter = express.Router();
 
-
-function handleErrors<P, ResBody, ReqBody, ReqQuery, Locals extends Record<string, any>>(
-	fn: (req: express.Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: express.Response<ResBody, Locals>, next?: express.NextFunction) => Promise<void> | void
-) {
-	return async (req: express.Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: express.Response<ResBody, Locals>, next: express.NextFunction) => {
-		try {
-			const response = fn(req, res, next);
-			if (response instanceof Promise) {
-				await response;
-			}
-		} catch (err) {
-			next(err);
-		}
-	}
-}
-
-studentRouter.get('', handleErrors(Student.getAll));
-studentRouter.get('/:id', handleErrors(Student.get));
-studentRouter.post('', handleErrors(Student.post));
-studentRouter.put('/:id', handleErrors(Student.put));
-studentRouter.delete('/:id', handleErrors(Student.remove));
-
 // TODO: Implement invalid route support
 // Example: POST /student/:id -> 404 NOT FOUND
+studentRouter.get('', Student.getAll);
+studentRouter.get('/:id', Student.get);
+studentRouter.post('', Student.post);
+studentRouter.put('/:id', Student.put);
+studentRouter.delete('/:id', Student.remove);
