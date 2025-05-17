c09e55ea899fa2328b002d16d58fb594d0f833c3
João Victor Olivo <jvso@yahoo.com>
2025-05-17 19:50:52 -0300

    ROUTER REFACTOR - Router implementing the refactored controller

--- src/router/student-router.ts
+++ src/router/student-router.ts
@@ -1,10 +1,26 @@
-import express from "express";
-import * as Student from "../controller/student-controller";
+import express, {Router} from "express";
+import { StudentController } from "../controller/student-controller";
+import {StudentService} from "../service/student-service";
 
-export const studentRouter = express.Router();
+let router: Router | undefined = undefined;
+let controller: StudentController | undefined = undefined;
 
-studentRouter.get('', Student.getAll);
-studentRouter.get('/:id', Student.get);
-studentRouter.post('', Student.post);
-studentRouter.put('/:id', Student.put);
-studentRouter.delete('/:id', Student.remove);
+
+export function getRouter() {
+    return router;
+}
+
+export async function requireRouter() {
+    if (router && controller) return router;
+
+    router = express.Router();
+    controller = new StudentController(await StudentService.new());
+
+    router.get('', controller.getAll);
+    router.get('/:id', controller.get);
+    router.post('', controller.post);
+    router.put('/:id', controller.put);
+    router.delete('/:id', controller.remove);
+
+    return router;
+}
