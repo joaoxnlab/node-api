12d06c42465b08895ee69d37b57ca1b43c99b379
João Victor Olivo <jvso@yahoo.com>
2025-05-17 19:51:52 -0300

    MAIN REFACTOR - Main file runs app asynchronously to implement refactored router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-import { HttpError } from "./infra/error/error-classes";
+import {HttpError} from "./infra/error/error-classes";
 
-import { loggerLevel, LogLevel } from './utils/logger';
-import { jsonParser } from './middleware/jsonParser';
-import { enableLoggedResponses, initRequestLogger } from './middleware/logs';
-import { errorHandler, jsonParserHandler, listenUnhandledRejections } from './infra/error/error-handler';
+import {loggerLevel, LogLevel} from './utils/logger';
+import {jsonParser} from './middleware/jsonParser';
+import {enableLoggedResponses, initRequestLogger} from './middleware/logs';
+import {errorHandler, jsonParserHandler, listenUnhandledRejections} from './infra/error/error-handler';
 
-import { studentRouter } from './router/student-router';
 import * as DevKit from "./.dev/develop-kit";
+import {requireRouter as requireStudentRouter} from "./router/student-router";
 
 // Set Level before executing other dependencies that might use the logger
 loggerLevel(LogLevel.INFO);
@@ -20,18 +20,22 @@ listenUnhandledRejections();
 const app = express();
 const PORT = 8800;
 
-app.use(jsonParser);
-app.use(jsonParserHandler);
+(async () => {
 
-app.use(initRequestLogger);
-app.use(enableLoggedResponses);
-app.use('/students', studentRouter);
-app.all('/{*path}', (req, _res, next) => {
-    next(new HttpError(404, `Router with Path '${req.originalUrl}' Not Found`));
-});
+    app.use(jsonParser);
+    app.use(jsonParserHandler);
 
-app.use(errorHandler);
+    app.use(initRequestLogger);
+    app.use(enableLoggedResponses);
+    app.use('/students', await requireStudentRouter());
+    app.all('/{*path}', (req, _res, next) => {
+        next(new HttpError(404, `Router with Path '${req.originalUrl}' Not Found`));
+    });
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+    app.use(errorHandler);
+
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    });
+
+})().then();
